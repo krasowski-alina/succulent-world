fcec232d4544701898a7ebabb006e055cc51fac7
“krasowski-alina” <alina.krasowski.web@gmail.com>
2023-05-01 13:45:05 -0400

    fixed home page route

--- src/App.js
+++ src/App.js
@@ -15,12 +15,12 @@ function App() {
     <div>
       <Router>
         <nav>
-          <Link to="home" className='link'><img src={icon} alt="company logo" className='logo'/></Link>
+          <Link to="/" className='link'><img src={icon} alt="company logo" className='logo'/></Link>
           <Link to="about" className='link'>About</Link>
           <Link to="contact" className='link'>Contact</Link>
         </nav>
         <Routes>
-          <Route path="home" element={<Home/>}/>
+          <Route path="/" element={<Home/>}/>
           <Route path="about" element={<About/>}/>
           <Route path="contact" element={<Contact/>}/>
         </Routes>
